fbc723d2ea5f3769f2c7541e034772fcddb45b3b
SLINN <sld8@psu.edu>
2026-02-20 11:34:47 -0500

    Add qpa_launch/qpa_list functions, update welcome message, fix unFBposts encoding

--- R/start.R
+++ R/start.R
@@ -1,5 +1,13 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to the PLSC 309 tutorials!")
+  packageStartupMessage(
+    "Welcome to QPATutorials!\n",
+    "These tutorials teach data analysis in R for political science,\n",
+    "covering R basics, univariate and bivariate description,\n",
+    "hypothesis testing, regression, and logistic regression.\n\n",
+    "To see all available tutorials, run: qpa_list()\n",
+    "To open a tutorial in your browser, run: qpa_launch(number)\n",
+    "Example: qpa_launch(1) opens R Basics Part 1"
+  )
   options(shiny.launch.browser = TRUE)
   options(tutorial.external.browser = TRUE)
 }
